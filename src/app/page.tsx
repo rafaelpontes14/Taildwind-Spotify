7a933e05578b59acc32aa66d6c99e9b0404b189a
rafaelpontes14 <rafafpontes@gmail.com>
2023-07-21 16:45:31 -0300

    new play button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { Home as HomeIcon, Search, Library, ChevronLeft, ChevronRight} from 'lucide-react'
+import { Home as HomeIcon, Search, Library, ChevronLeft, ChevronRight, Play} from 'lucide-react'
 
 export default function Home() {
   return (
@@ -52,34 +52,52 @@ export default function Home() {
             <h1 className="font-semibold text-3xl mt-10">Good Afternoon</h1>
 
             <div className="grid grid-cols-3 gap-4 mt-4">
-              <a href="#" className="bg-white/10 rounded flex items-center overflow-hidden gap-4 hover:bg-white/20 transition-colors">
+              <a href="#" className="bg-white/5 group rounded flex items-center overflow-hidden gap-4 hover:bg-white/10 transition-colors">
                 <img src="/album1.jpg" width={104} height={104} alt="Capa do álbum Stoney do cantor Post Malone" />
                 <strong>Stoney</strong>
+                <button className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
+                  <Play/>
+                </button>
               </a>
              
-              <a href="#" className="bg-white/10 rounded flex items-center overflow-hidden gap-4 hover:bg-white/20 transition-colors">
+              <a href="#" className="bg-white/5 group rounded flex items-center overflow-hidden gap-4 hover:bg-white/10 transition-colors">
                 <img src="/album1.jpg" width={104} height={104} alt="Capa do álbum Stoney do cantor Post Malone" />
                 <strong>Stoney</strong>
+                <button className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
+                  <Play/>
+                </button>
               </a>
               
-              <a href="#" className="bg-white/10 rounded flex items-center overflow-hidden gap-4 hover:bg-white/20 transition-colors"> 
+              <a href="#" className="bg-white/5 group rounded flex items-center overflow-hidden gap-4 hover:bg-white/10 transition-colors"> 
                 <img src="/album1.jpg" width={104} height={104} alt="Capa do álbum Stoney do cantor Post Malone" />
                 <strong>Stoney</strong>
+                <button className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
+                  <Play/>
+                </button>
               </a>
              
-              <a href="#" className="bg-white/10 rounded flex items-center overflow-hidden gap-4 hover:bg-white/20 transition-colors">
+              <a href="#" className="bg-white/5 group rounded flex items-center overflow-hidden gap-4 hover:bg-white/10 transition-colors">
                 <img src="/album1.jpg" width={104} height={104} alt="Capa do álbum Stoney do cantor Post Malone" />
                 <strong>Stoney</strong>
+                <button className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
+                  <Play/>
+                </button>
               </a>
              
-              <a href="#" className="bg-white/10 rounded flex items-center overflow-hidden gap-4 hover:bg-white/20 transition-colors">
+              <a href="#" className="bg-white/5 group rounded flex items-center overflow-hidden gap-4 hover:bg-white/10 transition-colors">
                 <img src="/album1.jpg" width={104} height={104} alt="Capa do álbum Stoney do cantor Post Malone" />
                 <strong>Stoney</strong>
+                <button className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
+                  <Play/>
+                </button>
               </a>
               
-              <a href="#" className="bg-white/10 rounded flex items-center overflow-hidden gap-4 hover:bg-white/20 transition-colors">
+              <a href="#" className="bg-white/5 group rounded flex items-center overflow-hidden gap-4 hover:bg-white/10 transition-colors">
                 <img src="/album1.jpg" width={104} height={104} alt="Capa do álbum Stoney do cantor Post Malone" />
                 <strong>Stoney</strong>
+                <button className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
+                  <Play/>
+                </button>
               </a>
             </div>
           </main>    
